d53f16e0efde378fdb974ccc83c41f9f4144dc66
Gabriel de Lima <114449624+LimaKsp@users.noreply.github.com>
2022-11-07 20:48:54 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,51 @@
+let imagemTroca = document.querySelector("#imagemTroca");
+let botoesSome = document.querySelector(".botoes");
+let navBar = document.querySelector(".nav1");
+
+function abrir() {
+  if (itens.style.display == "flex") {
+    itens.style.display = "none";
+    imagemTroca.setAttribute("src", "menu.png");
+    botoesSome.style.display = "flex";
+    navBar.style.borderBottom = "none";
+  } else {
+    itens.style.display = "flex";
+    imagemTroca.setAttribute("src", "menu-x.png");
+    botoesSome.style.display = "none";
+    navBar.style.borderBottom = "dashed 3px rgb(45, 247, 17, 255)";
+  }
+}
+function mudarTamanho() {
+  if (window.innerWidth >= 378) {
+    itens.style.display = "flex";
+  }
+}
+
+/* validação para o formulario de login */
+let valido = false;
+
+function validateform() {
+  // var Email= document.myform.Emailt.value;
+  var password = document.myform.password.value;
+  var x = document.myform.Email.value;
+  var atposition = x.indexOf("@");
+  var dotposition = x.lastIndexOf(".");
+
+  if (
+    atposition < 1 ||
+    dotposition < atposition + 2 ||
+    dotposition + 2 >= x.length
+  ) {
+    alert("Por favor,digite um Email válido");
+    return false;
+  } else if (password.length < 6) {
+    alert("Por favor,confira sua Senha");
+    return false;
+  }
+  valido = true;
+
+  if (valido === true) {
+    document.querySelector(".body").classList.add("escondido");
+    document.querrySelector(".tela2").classList.remove("escondido");
+  }
+}
